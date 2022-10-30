bd399a7e82478ba4010bd604080e7d6bdd5108d8
Majd Roaydi <majdroaydi@hotmail.com>
2022-10-30 15:55:53 +0200

    navcommon.h | Added more operators to Point_r

--- src/nav/navcommon.h
+++ src/nav/navcommon.h
@@ -10,40 +10,77 @@ namespace nav {
 
         inline Point_r(int_type x, int_type y) : x(x), y(y) {}
 
+
         Point_r<int_type> operator -() {
             return Point_r(-x, -y);
         }
+
 
         Point_r<int_type> operator +(int_type z) {
             return Point_r(x + z, y + z);
         }
 
-        Point_r<int_type> operator -(int_type z) {
-            return *this + -z;
-        }
+        Point_r<int_type> operator +(Point_r<int_type> z) {
+            return Point_r(x + z.x, y + z.y);
+        };
 
         Point_r<int_type> operator +=(int_type z) {
             return *this = *this + z;
-        }
-
-        Point_r<int_type> operator -=(int_type z) {
-            return *this = *this - z;
-        }
-
-        Point_r<int_type> operator +(Point_r<int_type> z) {
-            return Point_r(x + z.x, y + z.y);
-        }
-
-        Point_r<int_type> operator -(Point_r<int_type> z) {
-            return *this + -z;
         }
 
         Point_r<int_type> operator +=(Point_r<int_type> z) {
             return *this = *this + z;
         }
 
+
+        Point_r<int_type> operator -(int_type z) {
+            return *this + -z;
+        }
+
+        Point_r<int_type> operator -(Point_r<int_type> z) {
+            return *this + -z;
+        }
+
+        Point_r<int_type> operator -=(int_type z) {
+            return *this = *this - z;
+        }
+
         Point_r<int_type> operator -=(Point_r<int_type> z) {
             return *this = *this - z;
+        }
+        
+
+        Point_r<int_type> operator *(int_type z) {
+            return Point_r(x * z, y * z);
+        }
+
+        Point_r<int_type> operator *(Point_r<int_type> z) {
+            return Point_r(x * z.x, y * z.y);
+        };
+
+        Point_r<int_type> operator *=(int_type z) {
+            return *this = *this * z;
+        }
+
+        Point_r<int_type> operator *=(Point_r<int_type> z) {
+            return *this = *this * z;
+        }
+        
+
+        Point_r<int_type> operator /(int_type z) {
+            return Point_r(x / z, y / z);
+        }
+
+        Point_r<int_type> operator /(Point_r<int_type> z) {
+            return Point_r(x / z.x, y / z.y);
+        }
+
+        Point_r<int_type> operator /=(int_type z) {
+            return *this = *this / z;
+        }
+
+        Point_r<int_type> operator /=(Point_r<int_type> z) {
+            return *this = *this / z;
         }
     };
 
