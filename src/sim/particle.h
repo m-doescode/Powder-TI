18105776c18f197acdbbdd0577c8248aca36c60a
Majd Roaydi <majdroaydi@hotmail.com>
2022-11-05 19:07:35 +0200

    particle.g | IN_BOUNDS now actually makes sense... Not that it really matters

--- src/sim/particle.h
+++ src/sim/particle.h
@@ -2,7 +2,7 @@
 
 #include <stdint.h>
 
-#define IN_BOUNDS(x, y) (x >= 0 && y >= 0 && x < 100 && y < 100)
+#define IN_BOUNDS(x, y) (x >= 0 && y >= 0 && x <= SIM_W && y <= SIM_H)
 
 namespace sim {
     typedef int16_t PartId;
