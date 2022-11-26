defbf748910dbe448b372a63a94a9f578ddc589b
Majd Roaydi <majdroaydi@hotmail.com>
2022-11-26 16:13:42 +0200

    sim_renderer.cpp | Don't ask, just don't ask.

--- src/sim/sim_renderer.cpp
+++ src/sim/sim_renderer.cpp
@@ -6,46 +6,49 @@
 #include "particle.h"
 #include "sim.h"
 
+#include "common_externs.h"
 #include "sim_renderer.h"
 
-#define MAX_PARTS 1000
-#define SIM_W 100
-#define SIM_H 100
+
+#include <time.h>
 
 using std::vector;
 
-using sim::Particle;
-using sim::PartId;
 using sim::PartUpdate;
-
-extern Particle parts[MAX_PARTS];
-extern PartId pmap[SIM_W][SIM_H];
-extern uint16_t parts_num;
-extern PartId pfree;
-
-extern uint32_t frames;
 
 vector<PartUpdate> pupdates;
 
 void sim::render::render_sim() {
-    gfx_SetColor(gfx_RGBTo1555(0, 0, 0));
-    gfx_FillRectangle(4, 4, SIM_W * 2, SIM_H * 2);
+    //gfx_SetColor(gfx_RGBTo1555(0, 0, 0));
 
-    dbg_printf("Particle updates: %d\n", pupdates.size());
+    //dbg_printf("UPD: %d  PARTS: %d  EFF: %f\n", pupdates.size(), parts_num, (float)parts_num / pupdates.size());
+    while (pupdates.size() > 0) {
+        clock_t c = clock();
+        PartUpdate upd = pupdates[0];
+        uint8_t x = upd.x;
+        uint8_t y = upd.y;
 
-    gfx_SetColor(/*gfx_RGBTo1555(255, 224, 160)*/ 4);
-    int rp = 0; // Real parts
-    for (int p = 0; p < MAX_PARTS && rp < parts_num; p++) {
-        if (parts[p].type == 0)
-            continue;
-        rp++;
+        PartId pId = pmap[y][x];
+        uint8_t color = 0;
+        if (pId != 0xFF)
+            color = (parts[pId].type == 1) ? 4 : 0; // Update in M3/v0.3
 
-        Particle part = parts[p];
-        gfx_FillRectangle(part.x * 2 + 4, part.y * 2 + 4, 2, 2);
+        //dbg_printf("Updated (%d, %d) with %d\n", x, y, color);
+
+        gfx_SetColor(color);
+
+        gfx_FillRectangle(x * 2 + 4, y * 2 + 4, 2, 2);
     
+        clock_t a = clock();
+        pupdates.erase(pupdates.begin());
+        clock_t b = clock();
+        dbg_printf("%lu, %lu\n", b - a, b - c);
     }
 }
 
 void sim::render::update_part(uint8_t x, uint8_t y) {
-    pupdates.push_back({ x, y });
+    PartUpdate upd;
+    upd.x = x;
+    upd.y = y;
+    pupdates.push_back(upd);
 }
