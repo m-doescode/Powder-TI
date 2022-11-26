f871fea10a02115713f044d804d0c583c77a4418
Majd Roaydi <majdroaydi@hotmail.com>
2022-11-26 16:13:02 +0200

    Common externals ugghhhhh

---
+++ src/sim/common_externs.h
@@ -0,0 +1,8 @@
+#include "particle.h"
+#include "sim.h"
+
+extern sim::Particle parts[MAX_PARTS];
+extern sim::PartId pmap[SIM_W][SIM_H];
+extern uint16_t parts_num;
+extern sim::PartId pfree;
+extern uint32_t frames;
