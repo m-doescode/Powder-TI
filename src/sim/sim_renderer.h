ae127b6f2e2e093ae3ec7c03be6e939f4b5df72e
Majd Roaydi <majdroaydi@hotmail.com>
2022-12-04 15:00:39 +0200

    sim_renderer.h | Added method declarations

--- src/sim/sim_renderer.h
+++ src/sim/sim_renderer.h
@@ -1,5 +1,17 @@
-// #pragma once
+#pragma once
 
-// namespace sim::render {
-//     void render_sim();
-// }
+#include "sim.h"
+
+namespace sim::render {
+    // If the user unpauses rendering or if it REALLY is necessary
+    void redraw_all();
+    
+    /***
+     * Redraw ONE part. If partId == -1, then part is set to whatever is at that position. If the position is empty, it renders black.
+     * Otherwise, it will render the requested part at the specified position
+    */
+    void render_part(PartId partId, uint8_t x, uint8_t y);
+
+    // NYI, for toolbar
+    void redraw_toolbar();
+}
