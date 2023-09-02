e02950d8d361207f74e07e99a24761b89aabdb99
Majd Roaydi <majdroaydi@hotmail.com>
2023-09-03 00:31:36 +0300

    commerr.h | Filename included in errors

--- src/commerr.h
+++ src/commerr.h
@@ -2,11 +2,11 @@
 
 #ifdef TI84
     // #define throw(x, ...) { dbg_printf(__CONCAT("LINE %d: ", x), __LINE__, __VA_ARGS__); abort(); }
-    inline void throw_(unsigned int lineno, const char * format, ...) {
+    inline void throw_(const char* file, unsigned int lineno, const char * format, ...) {
         va_list argptr;
         va_start(argptr, format);
 
-        sprintf(dbgout, "LINE %d: ", lineno);
+        sprintf(dbgout, "%s@%d: ", file, lineno);
         vsprintf(dbgout, format, argptr);
         sprintf(dbgout, "\n");
 
@@ -15,7 +15,7 @@
         abort();
     }
 
-    #define throw(...) throw_(__LINE__, __VA_ARGS__)
+    #define throw(...) throw_(__FILE__, __LINE__, __VA_ARGS__)
 #else
     #define throw(x) throw std::string(x)
 #endif
